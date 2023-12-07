321d9aae18d0701d8b08fc40e7175ea38501dbc7
bamagid <bamagid60@gmail.com>
2023-12-07 13:16:20 +0000

    le crud des reponses est effective

--- app/Http/Controllers/ReponseController.php
+++ app/Http/Controllers/ReponseController.php
@@ -3,16 +3,18 @@
 namespace App\Http\Controllers;
 
 use App\Models\Reponse;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class ReponseController extends Controller
 {
-    /**
+  /**
      * Display a listing of the resource.
      */
     public function index()
     {
-        //
+        $reponses=Reponse::where('is_deleted', 0)->get();
+        return $reponses;
     }
 
     /**
@@ -28,21 +30,39 @@ class ReponseController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $request->validate(
+            [
+                'contenu' => 'required|string|min:3',
+                'user_id' => 'required|numeric',
+                'commentaire_id' => 'required|numeric',
+            ]
+
+        );
+        Reponse::create(
+            [
+                'contenu' => $request->contenu,
+                'user_id' => $request->input('user_id'),
+                'commentaire_id' => $request->input('commentaire_id')
+            ]
+        );
+        return response()->json(['message' => "La reponse est bien ajouté"]);
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(Reponse $reponse)
+    public function show(Request $request)
     {
-        //
+         $reponse = Reponse::findOrFail($request->input('id'));
+        if ($reponse && $reponse->is_deleted===0) {
+            return $reponse;
+        }
     }
 
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(Reponse $reponse)
+    public function edit(Reponse $Reponse)
     {
         //
     }
@@ -50,16 +70,59 @@ class ReponseController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, Reponse $reponse)
+    public function update(Request $request)
     {
-        //
+        $request->validate(
+            [
+                'contenu' => 'required|string|min:3',
+                'user_id' => 'required|numeric',
+                'id' => 'required|numeric'
+            ]
+        );
+        $reponse = Reponse::findOrFail($request->input('id'));
+        if ($reponse->user_id===$request->user_id) {
+            $reponse->contenu = $request->contenu;
+            $reponse->update();
+            return response()->json(['message' => "La reponse est bien modifié"]);
+        }
+        else{
+            return response()->json(['error'=>"Vous n'avez pas le droit de modifier cette reponse"],401);
+            }
+            
+    }
+    /**
+     * Archive the specified resource in storage.
+     */
+    public function archiveReponse(Request $request)
+    {
+        $request->validate(
+            [
+                'user_id' => 'required|numeric',
+                'id' => 'required|numeric'
+            ]
+        );
+        $reponse = Reponse::findOrFail($request->input('id'));
+        if ($reponse->user_id===$request->user_id) {
+        $reponse->is_deleted=true;
+        $reponse->update();
+        return response()->json(['message' => "La reponse est bien archivé"]);
+        }else{
+            return response()->json(['error'=>"Vous n'avez pas le droit de supprimer cette reponse"],401);
+            }
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(Reponse $reponse)
+    public function destroy(Request $request)
     {
-        //
+        $user=User::findOrFail($request->user_id);
+        if ($user->role_id===1){
+        $reponse = Reponse::findOrFail($request->input('id'));
+        $reponse->delete();
+        return response()->json(['message' => "La reponse est bien supprimé"]);
+    }else{
+        return response()->json(['error'=>"Vous n'avez pas les droits pour effectuer cette action"],401);
+        }
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\ForumController;
 use App\Http\Controllers\CommentaireController;
+use App\Http\Controllers\ReponseController;
 use App\Http\Controllers\RessourceController;
 use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
@@ -21,7 +22,7 @@ use Illuminate\Support\Facades\Route;
 
 /* gestion des forums*/
 //lister les forums
-Route::get('/forums',[ForumController::class,'index']);
+Route::get('/forums', [ForumController::class, 'index']);
 //creer un forum
 Route::post('/forum/create', [ForumController::class, 'store'])->name('forum.create');
 //supprimer un forum
@@ -29,7 +30,7 @@ Route::post('/forum/delete', [ForumController::class, 'destroy'])->name('forum.d
 //modifier un forum
 Route::post('forum/edit', [ForumController::class, 'update'])->name('forum.edit');
 //afficher le forum selectionne
-Route::post('/forum',[ForumController::class,'show'])->name('forum.show');
+Route::post('/forum', [ForumController::class, 'show'])->name('forum.show');
 //archiver une rubrique d'un forum
 Route::post('/forum/archiveRubrique', [ForumController::class, 'archiveRubrique'])->name('forum.archiveRubrique');
 /* gestion des commentaires du forums */
@@ -38,23 +39,38 @@ Route::get('/commentaires', [CommentaireController::class, 'index']);
 //lister  commentaire d'un forum
 Route::post('/commentaire', [CommentaireController::class, 'show']);
 //ajouter un commentaire a un forum
-Route::post('/commentaire/create', [CommentaireController::class, 'store'])->name('comment.add');
+Route::post('/commentaire/create', [CommentaireController::class, 'store'])->name('commentaire.add');
 //supprimer un commentaire d'un forum
-Route::post('/commentaire/delete', [CommentaireController::class, 'destroy'])->name('comment.delete');
+Route::post('/commentaire/delete', [CommentaireController::class, 'destroy'])->name('commentaire.delete');
 //modifier un commentaire d'un forum
-Route::post('/commentaire/edit', [CommentaireController::class, 'update'])->name('comment.edit');
+Route::post('/commentaire/edit', [CommentaireController::class, 'update'])->name('commentaire.edit');
 //archiver un commentaire de forum
-Route::post('/commentaire/archive', [CommentaireController::class, 'archiveCommentaire'])->name('comment.archive');
+Route::post('/commentaire/archive', [CommentaireController::class, 'archiveCommentaire'])->name('commentaire.archive');
+
+
+/* gestion des reponses du forums */
+//lister tous les reponses d'un forum
+Route::get('/reponses', [ReponseController::class, 'index']);
+//lister  reponse d'un forum
+Route::post('/reponse', [ReponseController::class, 'show']);
+//ajouter un reponse a un forum
+Route::post('/reponse/create', [ReponseController::class, 'store'])->name('reponse.add');
+//supprimer un reponse d'un forum
+Route::post('/reponse/delete', [ReponseController::class, 'destroy'])->name('reponse.delete');
+//modifier un reponse d'un forum
+Route::post('/reponse/edit', [ReponseController::class, 'update'])->name('reponse.edit');
+//archiver un reponse de forum
+Route::post('/reponse/archive', [ReponseController::class, 'archivereponse'])->name('reponse.archive');
 
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
-Route::post('ajouter-ressource',[RessourceController::class,'ajouterRessource'])->name('ajouter-ressource');
+Route::post('ajouter-ressource', [RessourceController::class, 'ajouterRessource'])->name('ajouter-ressource');
 Route::put('/ressources/{id}', [RessourceController::class, 'modifierRessource']);
-    Route::delete('/ressources/{id}', [RessourceController::class, 'supprimerRessource']);
-    Route::post('/ajouter-role', [UserController::class, 'ajouterRole']);
-    Route::post('/ajouter-utilisateur-entrepreneur-novice', [UserController::class, 'ajouterUtilisateurEntrepreneurNovice']);
-    Route::post('/ajouter-utilisateur-entrepreneur-experimente', [UserController::class,'ajouterUtilisateurEntrepreneurExperimente']);
-    Route::post('/ajouter-utilisateur-admin', [UserController::class,'ajouterUtilisateurAdmin']);
-    Route::post('login', [UserController::class, 'login']);
+Route::delete('/ressources/{id}', [RessourceController::class, 'supprimerRessource']);
+Route::post('/ajouter-role', [UserController::class, 'ajouterRole']);
+Route::post('/ajouter-utilisateur-entrepreneur-novice', [UserController::class, 'ajouterUtilisateurEntrepreneurNovice']);
+Route::post('/ajouter-utilisateur-entrepreneur-experimente', [UserController::class, 'ajouterUtilisateurEntrepreneurExperimente']);
+Route::post('/ajouter-utilisateur-admin', [UserController::class, 'ajouterUtilisateurAdmin']);
+Route::post('login', [UserController::class, 'login']);
